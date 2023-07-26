9faef880eae31268872db70b6e2754b50b533ae7
jasonjjjc <57393155+jasonjjjc@users.noreply.github.com>
2023-07-26 14:57:23 +0100

    finished form submission javascript

--- js/main.js
+++ js/main.js
@@ -18,6 +18,9 @@ $(document).ready(function () {
 
 
 
+
+
+
 // make the header menu appear and disappear when the menu button is clicked
 
 let menuWrapper = document.getElementById('menu-wrapper');
@@ -32,6 +35,10 @@ menuWrapper.addEventListener('click', () => {
 
 
 
+
+
+
+
 // toggle the is-active class on the header element when the screen width is large or wider
 
 function toggleIsActiveClass() {
@@ -39,7 +46,7 @@ function toggleIsActiveClass() {
   const screenWidth = window.innerWidth;
 
   const largeScreenWidth = 992;
-  
+
   // only add the is-active class if the screen width is greater than or equal to 992px and the is-active class is not already present
   if (screenWidth >= largeScreenWidth && !header.classList.contains('is-active')) {
     header.classList.add('is-active');
@@ -54,3 +61,90 @@ window.addEventListener('resize', toggleIsActiveClass);
 
 
 
+
+
+
+
+
+
+
+// Perform input validation on all contact form inputs and ensure that the email address is in a valid formatusing regex
+
+// Get the form and submit button elements
+const contactForm = document.getElementById('contact-form');
+const submitButton = document.getElementById('contact-form-submit');
+
+// Regular expression for email validation
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// Function to display validation error messages below the input fields
+function showValidationError(inputField, message) {
+  const errorDiv = document.createElement('div');
+  errorDiv.className = 'validation-error';
+  errorDiv.textContent = message;
+  inputField.parentNode.appendChild(errorDiv);
+}
+
+// Function to remove validation error messages
+function removeValidationError(inputField) {
+  const errorDiv = inputField.parentNode.querySelector('.validation-error');
+  if (errorDiv) {
+    errorDiv.remove();
+  }
+}
+
+// Add event listener to the form's submit button
+submitButton.addEventListener('click', function (event) {
+  // Prevent the form from submitting by default
+  event.preventDefault();
+
+  // Get the values of the form fields
+  const firstName = document.getElementById('first-name').value;
+  const lastName = document.getElementById('last-name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('contact-form-inputs-message').value;
+
+  // Remove any existing validation error messages
+  const inputFields = [document.getElementById('first-name'), document.getElementById('last-name'), document.getElementById('email'), document.getElementById('contact-form-inputs-message')];
+  inputFields.forEach(inputField => {
+    removeValidationError(inputField);
+  });
+
+  // Perform basic validation
+  if (firstName.trim() === '') {
+    showValidationError(document.getElementById('first-name'), 'Please enter your first name.');
+    return;
+  }
+
+  if (lastName.trim() === '') {
+    showValidationError(document.getElementById('last-name'), 'Please enter your last name.');
+    return;
+  }
+
+  if (email.trim() === '') {
+    showValidationError(document.getElementById('email'), 'Please enter your email address.');
+    return;
+  }
+
+  // Check if the email is in a valid format using the regex
+  if (!emailRegex.test(email)) {
+    showValidationError(document.getElementById('email'), 'Please enter a valid email address.');
+    return;
+  }
+
+  if (message.trim() === '') {
+    showValidationError(document.getElementById('contact-form-inputs-message'), 'Please enter your message.');
+    return;
+  }
+
+  // If all validation passes, submit the form
+  // contactForm.submit();
+  // append confirmation message to the form
+  const confirmationMessage = document.createElement('p');
+  confirmationMessage.className = 'confirmation-message';
+  confirmationMessage.textContent = 'Thank you for your message. We will get back to you as soon as possible.';
+  contactForm.appendChild(confirmationMessage);
+  // remove the form
+  contactForm.remove();
+
+});
